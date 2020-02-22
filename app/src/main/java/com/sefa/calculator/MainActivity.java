15fdff318241daa7102b6d94bac6aac566bdf0f6
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2020-02-23 00:17:04 +0300

    Completed Project

--- app/src/main/java/com/sefa/calculator/MainActivity.java
+++ app/src/main/java/com/sefa/calculator/MainActivity.java
@@ -3,12 +3,74 @@ package com.sefa.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText number1;
+    EditText number2;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        number1=findViewById(R.id.number1);
+        number2=findViewById(R.id.number2);
+        result=findViewById(R.id.result);
+    }
+
+    public void plus(View view){
+
+        if (number1.getText().toString().matches("")|| number2.getText().toString().matches("")){
+            result.setText("Enter Number!");
+        }
+        else{
+            int n1 = Integer.parseInt(number1.getText().toString());
+            int n2 = Integer.parseInt(number2.getText().toString());
+            int finaly = n1 + n2;
+            result.setText("Result : " + finaly);
+        }
+    }
+
+    public void minus(View view){
+
+        if (number1.getText().toString().matches("")|| number2.getText().toString().matches("")){
+            result.setText("Enter Number!");
+        }
+        else{
+            int n1 = Integer.parseInt(number1.getText().toString());
+            int n2 = Integer.parseInt(number2.getText().toString());
+            int finaly = n1 - n2;
+            result.setText("Result : " + finaly);
+        }
+    }
+
+    public void times(View view){
+
+        if (number1.getText().toString().matches("")|| number2.getText().toString().matches("")){
+            result.setText("Enter Number!");
+        }
+        else{
+            int n1 = Integer.parseInt(number1.getText().toString());
+            int n2 = Integer.parseInt(number2.getText().toString());
+            int finaly = n1 * n2;
+            result.setText("Result : " + finaly);
+        }
+    }
+
+    public void divided(View view){
+
+        if (number1.getText().toString().matches("")|| number2.getText().toString().matches("")){
+            result.setText("Enter Number!");
+        }
+        else{
+            int n1 = Integer.parseInt(number1.getText().toString());
+            int n2 = Integer.parseInt(number2.getText().toString());
+            int finaly = n1 / n2;
+            result.setText("Result : " + finaly);
+        }
     }
 }
